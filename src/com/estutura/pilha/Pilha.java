64d63c94ef5d5d34835443a0e6e7856132635641
Dh3kon <kb-max@hotmail.com>
2017-01-21 22:26:09 -0200

    03
    21/01/2017

---
+++ src/com/estutura/pilha/Pilha.java
@@ -0,0 +1,14 @@
+package com.estutura.pilha;
+
+import com.estutura.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica{
+
+	public Pilha() {
+		super();
+	}
+	
+	public Pilha(int capacidade){
+		super(capacidade);
+	}
+}
---
+++ src/com/estutura/pilha/exercicios/Aula13.java
@@ -0,0 +1,13 @@
+package com.estutura.pilha.exercicios;
+
+import com.estutura.pilha.Pilha;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+		
+		Pilha<Integer> pilha = new Pilha<>();
+		System.out.println(pilha);
+		System.out.println(pilha.tamanho());
+	}
+}
