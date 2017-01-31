40de7c871b81e94b6600afd1018515ded6f22316
Dh3kon <kb-max@hotmail.com>
2017-01-30 22:05:24 -0200

    05
    30/01/17

---
+++ src/com/estutura/pilha/exercicios/Exer02.java
@@ -0,0 +1,58 @@
+package com.estutura.pilha.exercicios;
+
+import java.util.Scanner;
+
+import com.estutura.pilha.Pilha;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+
+		Pilha<Integer> par = new Pilha<>();
+		Pilha<Integer> impar = new Pilha<>();
+		Scanner scan = new Scanner(System.in);
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Digite um número: ");
+			int num = scan.nextInt();
+
+			if (num == 0) {
+				// Pilha par
+				Integer desempilhado = par.desempilha();
+
+				if (desempilhado == null) {
+					System.out.println("Pilha par vazia");
+				} else {
+					System.out.println("Desempilhando da pilha par: " + desempilhado);
+				}
+
+				// Pilha impar
+				desempilhado = impar.desempilha();
+
+				if (desempilhado == null) {
+					System.out.println("Pilha impar vazia");
+				} else {
+					System.out.println("Desempilhando da pilha impar: " + desempilhado);
+				}
+			} else if (num % 2 == 0) {
+				System.out.println("Número par, empilhando na pilha par " + num);
+				par.empilha(num);
+			} else {
+				System.out.println("Número par, empilhando na pilha par " + num);
+				impar.empilha(num);
+			}
+		}
+
+		System.out.println("Desempilhando todos os números da pilha par");
+
+		while (!par.estaVazia()) {
+			System.out.println("Desempilhando da pilha par: " + par.desempilha());
+		}
+
+		System.out.println("Desempilhando todos os números da pilha impar");
+
+		while (!impar.estaVazia()) {
+			System.out.println("Desempilhando da pilha impar: " + impar.desempilha());
+		}
+	}
+}
